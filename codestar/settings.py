d27b16eecc5eb5fe296348a6547f5981aa5558bd
Derek Garnett <rizzagum@gmail.com>
2024-07-11 22:03:03 +0000

    feat adds security

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yx%7+tvlf77*uj=827zj7h5(e7*7%f@af&=p440ako+&^fbs!a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
