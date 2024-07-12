5c34e83afe9783150a155bec5dfd78b61bc189ae
Derek Garnett <rizzagum@gmail.com>
2024-07-12 19:24:57 +0000

    FEAT adds comment db and model to posts

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,18 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")  # noqa
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     STATUS = ((0, "Draft"), (1, "Published"))
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # noqa
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')  # noqa
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
