9540072a5c751a481adbed60b2b3b724c830cd1b
Derek Garnett <rizzagum@gmail.com>
2024-07-14 20:22:41 +0000

    FEAT adds commrnts model and migration to sqlite

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return (f"{self.title} | written by {self.author}")
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # noqa
@@ -22,3 +28,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return (f"COMMENT {self.body} by {self.author}")
+# migrate blog zero
